6338590c0801a3a3a5fbefc98963d06f2c4c112f
raimundo-oliveira <raimundo2014721@gmail.com>
2024-06-11 15:34:07 -0300

    adição metodos

---
+++ java-basico/src/edu/raimundo/metodos/Metodos.java
@@ -0,0 +1,17 @@
+package edu.raimundo.metodos;
+
+public class Metodos {
+    /*
+     * Todo método deve ser nomeado como verbo;
+     * Seguir padrão camelCase (Todas as letras minúsculas exceto a primeira letra da segunda palavra);
+     * Cada método deve representar uma única responsabilidade.
+    */
+
+    public double somar(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+
+    public void imprimir(String texto){
+        System.out.println(texto);
+    }
+}
