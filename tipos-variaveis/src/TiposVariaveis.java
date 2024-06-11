e0b4754f8a0aa97b1fc434f18c6a49a21c23742a
raimundo-oliveira <raimundo2014721@gmail.com>
2024-06-11 12:17:45 -0300

    variaveis e constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+        long cpf = 938483948728349L;
+        float pi = 3.14F;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        // Variáveis
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+
+        // Constantes(nunca mudará o valor e deve ser escrita em caixa alta)
+        final double PI = 3.14;
     }
 }
