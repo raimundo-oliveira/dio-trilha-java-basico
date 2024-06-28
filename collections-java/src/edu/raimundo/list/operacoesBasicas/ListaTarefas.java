265be430e44adbe9a185dd9e5144d15abb5ec367
raimundo-oliveira <raimundo2014721@gmail.com>
2024-06-28 11:45:36 -0300

    Adicionando conteúdo de List

---
+++ collections-java/src/edu/raimundo/list/operacoesBasicas/ListaTarefas.java
@@ -0,0 +1,51 @@
+package edu.raimundo.list.operacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    private List<Tarefa> tarefaList;
+    
+    
+    public ListaTarefas(){
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equals(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    } 
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+
+        System.out.println("O número total de elementos na lista é: "+ listaTarefas.obterNumeroTotalTarefas());
+        
+        listaTarefas.adicionarTarefa("Tarefa 1");
+        listaTarefas.adicionarTarefa("Tarefa 2");
+        listaTarefas.adicionarTarefa("Tarefa 2");
+        
+        System.out.println("O número total de elementos na lista é: "+ listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.removerTarefa("Tarefa 1");
+        System.out.println("O número total de elementos na lista é: "+ listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.obterDescricoesTarefas();
+    }
+}
